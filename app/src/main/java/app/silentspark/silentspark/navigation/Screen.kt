3574cfc15221770823e41d35f738f768d1cf6165
ADMIN <2112110023@mhs.uqgresik.ac.id>
2024-06-21 22:05:23 +0700

    upload to new branch

---
+++ app/src/main/java/app/silentspark/silentspark/navigation/Screen.kt
@@ -0,0 +1,13 @@
+package app.silentspark.silentspark.navigation
+
+sealed class Screen (val route: String){
+    data object Home : Screen("home")
+    data object Splash : Screen("spalsh")
+    data object Kelas: Screen("kelas")
+    data object DetailKelas: Screen("detail_kelas")
+    data object Pesanan: Screen("daftar_pesanan")
+    data object TandaTerima: Screen("tanda_terima")
+    data object Akun: Screen("akun")
+    data object Login: Screen("login")
+    data object SignUp: Screen("signup")
+}
